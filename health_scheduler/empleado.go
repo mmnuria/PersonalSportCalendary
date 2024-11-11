9fbfb37bf5b383a061f28efe8db0e389548d947a
mmnuria <mmnuria@correo.ugr.es>
2024-11-11 14:31:14 +0100

    Solucion #9 arreglar errores y omisión de función de verificar dia libre, ya que es parte de la lógica de negocio no del modelo.

--- health_scheduler/empleado.go
+++ health_scheduler/empleado.go
@@ -1,30 +1,19 @@
 package health_scheduler
 
 type Empleado struct {
-    Nombre       string
-    TipoContrato Contrato
-    Turno        Turno
-    Area         Area
-    Disponibilidad []string
+	Nombre         string
+	TipoContrato   Contrato
+	Turno          Turno
+	Area           Area
+	Disponibilidad []string
 }
 
-// Función para verificar si un empleado tiene el día libre
-func (e *Empleado) EsDiaLibre(dia string) bool {
-    for _, diaLibre := range e.Disponibilidad {
-        if diaLibre == dia {
-            return true
-        }
-    }
-    return false
+func NuevoEmpleado(nombre string, tipoContrato Contrato, turno Turno, area Area, disponibilidad []string) *Empleado {
+	return &Empleado{
+		Nombre:         nombre,
+		TipoContrato:   tipoContrato,
+		Turno:          turno,
+		Area:           area,
+		Disponibilidad: disponibilidad,
+	}
 }
-
-func NuevoEmpleado(nombre, tipoContrato Contrato, turno Turno, area Area, disponibilidad []string) *Empleado {
-    return &Empleado{
-        Nombre:       nombre,
-        TipoContrato: tipoContrato,
-        Turno:        turno,
-        Area:         area,
-        Disponibilidad:   disponibilidad,
-    }
-}
-
