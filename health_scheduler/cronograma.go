1f94470c2af656114d4bc92a006d9dc71301d58a
mmnuria <mmnuria@correo.ugr.es>
2024-11-11 14:31:42 +0100

    Solucion #9 errores de cambio de nombre

--- health_scheduler/cronograma.go
+++ health_scheduler/cronograma.go
@@ -1,29 +1,29 @@
 package health_scheduler
 
 type Asignacion struct {
-    Empleado Empleado
-    Area     Area
-    Turno    Turno
-    EsFlexible  bool
+	Empleado   Empleado
+	Area       Area
+	Turno      Turno
+	EsFlexible bool
 }
 
 type CronogramaDiario struct {
-    Fecha       string
-    Asignaciones []Asignacion
+	Fecha        string
+	Asignaciones []Asignacion
 }
 
-func NuevaAsignacion(empleado Empleado, area Area, turno Turno, esFlexible bool) *Cronograma {
-    return &Asignacion{
-        Empleado: empleado,
-        Area:     area,
-        Turno:    turno,
-        EsFlexible: esFlexible,
-    }
+func NuevaAsignacion(empleado Empleado, area Area, turno Turno, esFlexible bool) *Asignacion {
+	return &Asignacion{
+		Empleado:   empleado,
+		Area:       area,
+		Turno:      turno,
+		EsFlexible: esFlexible,
+	}
 }
 
 func NuevoCronogramaDiario(fecha string, asignaciones []Asignacion) CronogramaDiario {
 	return CronogramaDiario{
-		Fecha:       fecha,
+		Fecha:        fecha,
 		Asignaciones: asignaciones,
 	}
 }
