2e558c0c40682d02fa2e15c00ff56fb0978a84ab
mmnuria <mmnuria@correo.ugr.es>
2024-11-11 11:02:17 +0100

    Soluciona #8 creando un struct que contiene la información de cada turno

---
+++ healthScheduler/turno.go
@@ -0,0 +1,31 @@
+package healthScheduler
+
+type Turno struct {
+    Nombre TipoTurno
+    Inicio string
+    Fin    string
+}
+
+func NuevoTurno(nombre TipoTurno) *Turno {
+
+	var inicio, fin string
+
+	switch nombre {
+	case Mañana:
+		inicio = "08:00"
+		fin = "15:00"
+	case Tarde:
+		inicio = "15:00"
+		fin = "22:00"
+	case Noche:
+		inicio = "22:00"
+		fin = "08:00"
+	}
+
+	return &Turno{
+        Nombre: nombre,
+        Inicio: inicio,
+        Fin:    fin,
+    }
+        
+}
