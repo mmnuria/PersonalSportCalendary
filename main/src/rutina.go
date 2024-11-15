495f8d4c24744a5628c7e0d5eeceef9d7ce3f11b
abbonno <abbogonluq@gmail.com>
2024-11-15 13:52:57 +0100

    #12 rutina creada

---
+++ main/src/rutina.go
@@ -0,0 +1,8 @@
+package PersonalSportCalendary
+
+type Rutina struct {
+	Nombre     string   // Nombre de la rutina
+	Duracion   int      // Duración en minutos
+	Ejercicios []string // Lista de ejercicios
+	Intensidad string   // Intensidad: "baja", "media", "alta"
+}
