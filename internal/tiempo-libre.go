96770d210bd3b03476bb5c9bbf1e87d720a53c8d
abbonno <abbogonluq@gmail.com>
2024-11-19 23:19:06 +0100

    Modificación tiempo libre #13

--- internal/tiempo-libre.go
+++ internal/tiempo-libre.go
@@ -1,8 +1,22 @@
 package PersonalSportCalendary
 
+type Dia int //Enum de días (para poder referenciarlos por su nombre)
+const (
+	Lunes Dia = iota
+	Martes
+	Miércoles
+	Jueves
+	Viernes
+	Sábado
+	Domingo
+)
+
+type IntervaloHorario struct { //Intervalos de tiempo libre (formato de los string "HH:MM")
+	HorarioInicio string
+	HorarioFin    string
+}
+
 type TiempoLibre struct {
-	Dia           string   // Día de la semana
-	HorarioInicio string   // Hora de inicio, formato "HH:MM"
-	HorarioFin    string   // Hora de fin, formato "HH:MM"
-	Restricciones []string // Restricciones opcionales
+	Dias     []Dia                      //Días como enum
+	Horarios map[Dia][]IntervaloHorario //Mapeamos los horarios de disponibilidad a su día correspondiente
 }
