483111ad5280fd9704c12b4745cff4e082df6395
abbonno <abbogonluq@gmail.com>
2024-11-19 23:18:03 +0100

    Modificación rutina según rutina-ejercicios.md #12

--- internal/rutina.go
+++ internal/rutina.go
@@ -1,8 +1,8 @@
 package PersonalSportCalendary
 
 type Rutina struct {
-	Nombre     string   // Nombre de la rutina
-	Duracion   int      // Duración en minutos
-	Ejercicios []string // Lista de ejercicios
-	Intensidad string   // Intensidad: "baja", "media", "alta"
+	Nombre     string   //Nombre de la rutina
+	Tiempo     int      //Duración en minutos
+	Ejercicios []string //Instrucciones detalladas sobre los ejercicios que componen la rutina y sus repeticiones
+	Materiales []string //Material necesario para realizar cada ejercicio de la rutina
 }
