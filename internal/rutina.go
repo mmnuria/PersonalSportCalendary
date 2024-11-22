159f78ce8016c1af3e116fef1a0b5714d6bbd590
abbonno <abbogonluq@gmail.com>
2024-11-22 15:04:27 +0100

    Añade el dato material para las rutinas #12

--- internal/rutina.go
+++ internal/rutina.go
@@ -1,15 +1,39 @@
 package PersonalSportCalendary
 
+type Material int //enum de materiales
+
+const (
+	Barbell = iota
+	Dumbbells
+	Bodyweight
+	Machine
+	MedicineBall
+	Kettlebells
+	Stretches
+	Cables
+	Band
+	Plate
+	TRX
+	Yoga
+	BosuBall
+	Vitruvian
+	Cardio
+	SmithMachine
+	Recovery
+)
+
 type Rutina struct {
 	Nombre     string      //Nombre de la rutina
 	Tiempo     int         //Duración en minutos
 	Ejercicios []Ejercicio //Instrucciones detalladas sobre los ejercicios que componen la rutina y sus repeticiones
+	Materiales []Material  //Materiales necesarios para realizar la rutina
 }
 
-func NewRutina(nombre string, tiempo int, ejercicios []Ejercicio, materiales []string) Rutina {
+func NewRutina(nombre string, tiempo int, ejercicios []Ejercicio, materiales []Material) Rutina {
 	return Rutina{
 		Nombre:     nombre,
 		Tiempo:     tiempo,
 		Ejercicios: ejercicios,
+		Materiales: materiales,
 	}
 }
