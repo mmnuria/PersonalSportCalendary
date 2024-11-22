e696e007b3d1dd4cd149655ad194d9661f000f61
abbonno <abbogonluq@gmail.com>
2024-11-22 14:58:13 +0100

    Reestructura tiempo libre, no es necesario guardar intervalos, solo tiempo libre a lo largo de la semana #13

--- internal/tiempo-libre.go
+++ internal/tiempo-libre.go
@@ -1,36 +1,13 @@
 package PersonalSportCalendary
 
-type Dia int //Enum de días (para poder referenciarlos por su nombre)
-const (
-	Lunes Dia = iota
-	Martes
-	Miércoles
-	Jueves
-	Viernes
-	Sábado
-	Domingo
-)
-
-type IntervaloHorario struct { //Intervalos de tiempo libre (formato de los string "HH:MM")
-	HorarioInicio string
-	HorarioFin    string
-}
-
-func NewIntervaloHorario(horarioInicio, horarioFin string) IntervaloHorario {
-	return IntervaloHorario{
-		HorarioInicio: horarioInicio,
-		HorarioFin:    horarioFin,
-	}
-}
-
 type TiempoLibre struct {
-	Dias     []Dia                      //Días como enum
-	Horarios map[Dia][]IntervaloHorario //Mapeamos los horarios de disponibilidad a su día correspondiente
+	Días   int //Días libres a la semana
+	Tiempo int //Tiempo en minutos
 }
 
-func NewTiempoLibre(dias []Dia, horarios map[Dia][]IntervaloHorario) TiempoLibre {
+func NewTiempoLibre(días int, tiempo int) TiempoLibre {
 	return TiempoLibre{
-		Dias:     dias,
-		Horarios: horarios,
+		Días:   días,
+		Tiempo: tiempo,
 	}
 }
