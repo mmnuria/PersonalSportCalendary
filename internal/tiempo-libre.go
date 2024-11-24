942df6b178cfa8e2adbd31a423335c670f6b1270
abbonno <abbogonluq@gmail.com>
2024-11-24 21:16:41 +0100

    Comprueba tiempo en minutos, tiempo libre #13

--- internal/tiempo-libre.go
+++ internal/tiempo-libre.go
@@ -1,11 +1,18 @@
 package PersonalSportCalendary
 
+import "fmt"
+
 type TiempoLibre struct {
 	Días   int //Días libres a la semana
 	Tiempo int //Tiempo en minutos
 }
 
 func NewTiempoLibre(días int, tiempo int) TiempoLibre {
+	if tiempo <= 0 || tiempo > 1440 {
+		fmt.Printf("El tiempo debe estar entre 1 y 1440 minutos, recibido: %d", tiempo)
+		return TiempoLibre{}
+	}
+
 	return TiempoLibre{
 		Días:   días,
 		Tiempo: tiempo,
