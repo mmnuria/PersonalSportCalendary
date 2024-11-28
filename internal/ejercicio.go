a6de0400b658b379a8734013c5e41c0bfe5488d8
abbonno <abbogonluq@gmail.com>
2024-11-28 13:51:00 +0100

    Elimina tipos enum innecesarios y cambia los atributos por strings
    Se eliminan los objetos ClaseEjercicio, Intensidad y Material ya que basta con crear comprobaciones en los propios constructores de que el dato es correcto, dentro del dominio definido

--- internal/ejercicio.go
+++ internal/ejercicio.go
@@ -3,53 +3,33 @@ package PersonalSportCalendary
 import "fmt"
 
 type Ejercicio struct {
-	Nombre        string         //Nombre del ejercicio
-	Descripcion   string         //Descripción del ejercicio
-	MinsEstimados int            //Tiempo estimado en minutos para realizar el ejercicio
-	TipoEjercicio ClaseEjercicio //Grupo muscular al que pertenece el ejercicio
-	Intensidad    Intensidad     //Intensidad del ejercicio
+	Nombre         string //Nombre del ejercicio
+	Descripcion    string //Descripción del ejercicio
+	MinsEstimados  int    //Tiempo estimado en minutos para realizar el ejercicio
+	ClaseEjercicio string //Clase de ejercicio a realizar entre: Flexibilidad, Fuerza, Cardiovasculares, Calentamiento, Estiramiento
+	Intensidad     string //Intensidad del ejercicio entre: Baja, Media, Alta
 }
 
-func NewEjercicio(nombre string, descripcion string, minsEstimados int, tipoEjercicio ClaseEjercicio, intensidad Intensidad) Ejercicio {
+func NewEjercicio(nombre string, descripcion string, minsEstimados int, claseEjercicio string, intensidad string) (Ejercicio, error) {
+	//Comprobación de clase de ejercicio
+	switch string(claseEjercicio) {
+	case "Flexibilidad", "Fuerza", "Cardiovasculares", "Calentamiento", "Estiramiento":
+		break
+	default:
+		return Ejercicio{}, fmt.Errorf("wjercicio inválido: %v", claseEjercicio)
+	}
+	//Comprobación de intensidad
+	switch string(intensidad) {
+	case "Baja", "Media", "Alta":
+		break
+	default:
+		return Ejercicio{}, fmt.Errorf("intensidad inválida: %v", intensidad)
+	}
 	return Ejercicio{
-		Nombre:        nombre,
-		Descripcion:   descripcion,
-		MinsEstimados: minsEstimados,
-		TipoEjercicio: tipoEjercicio,
-		Intensidad:    intensidad,
-	}
-}
-
-type ClaseEjercicio string //Enum de tipos de ejercicios
-const (
-	Flexibilidad     = "Flexibilidad"
-	Fuerza           = "Fuerza"
-	Cardiovasculares = "Cardiovasculares"
-	Calentamiento    = "Calentamiento"
-	Estiramiento     = "Estiramiento"
-)
-
-func NewClaseEjercicio(claseEjercicio string) (ClaseEjercicio, error) {
-	switch ClaseEjercicio(claseEjercicio) {
-	case Flexibilidad, Fuerza, Cardiovasculares, Calentamiento, Estiramiento:
-		return ClaseEjercicio(claseEjercicio), nil
-	default:
-		return "", fmt.Errorf("Material inválido: %v", claseEjercicio)
-	}
-}
-
-type Intensidad string //Enum de intensidades
-const (
-	Baja  = "Baja"
-	Media = "Media"
-	Alta  = "Alta"
-)
-
-func NewIntensidad(intensidad string) (Intensidad, error) {
-	switch Intensidad(intensidad) {
-	case Baja, Media, Alta:
-		return Intensidad(intensidad), nil
-	default:
-		return "", fmt.Errorf("Intensidad inválida: %v", intensidad)
-	}
+		Nombre:         nombre,
+		Descripcion:    descripcion,
+		MinsEstimados:  minsEstimados,
+		ClaseEjercicio: claseEjercicio,
+		Intensidad:     intensidad,
+	}, nil
 }
--- internal/rutina.go
+++ internal/rutina.go
@@ -6,32 +6,22 @@ type Rutina struct {
 	Nombre     string      //Nombre de la rutina
 	Tiempo     int         //Duración en minutos
 	Ejercicios []Ejercicio //Instrucciones detalladas sobre los ejercicios que componen la rutina y sus repeticiones
-	Materiales []Material  //Materiales necesarios para realizar la rutina
+	Materiales []string    //Materiales necesarios para realizar la rutina entre: Peso, BandaElastica, Cuerda, Esterilla, Pelota
 }
 
-func NewRutina(nombre string, tiempo int, ejercicios []Ejercicio, materiales []Material) Rutina {
+func NewRutina(nombre string, tiempo int, ejercicios []Ejercicio, materiales []string) (Rutina, error) {
+	//Comprobación de materiales válidos
+	for _, m := range materiales {
+		switch string(m) {
+		case "Peso", "BandaElastica", "Cuerda", "Esterilla", "Pelota":
+		default:
+			return Rutina{}, fmt.Errorf("material inválido: %v", m)
+		}
+	}
 	return Rutina{
 		Nombre:     nombre,
 		Tiempo:     tiempo,
 		Ejercicios: ejercicios,
 		Materiales: materiales,
-	}
-}
-
-type Material string //enum de materiales
-const (
-	Peso          = "Peso"
-	BandaElastica = "Banda Elastica"
-	Cuerda        = "Cuerda"
-	Esterilla     = "Esterilla"
-	Pelota        = "Pelota"
-)
-
-func NewMaterial(material string) (Material, error) {
-	switch Material(material) {
-	case Peso, BandaElastica, Cuerda, Esterilla, Pelota:
-		return Material(material), nil
-	default:
-		return "", fmt.Errorf("Material inválido: %v", material)
-	}
+	}, nil
 }
