a913cee9d6cb8172bf0fea8d22172bde1226c7de
abbonno <abbogonluq@gmail.com>
2024-12-02 11:48:05 +0100

    Añade ListaPlanesSemanales
    ListaPlanesSemanales almacena todos los planes semanales creados para los usuarios, de modo que se puedan guardar y manejar desde el mismo

--- internal/plan-semanal.go
+++ internal/plan-semanal.go
@@ -2,6 +2,9 @@ package PersonalSportCalendary
 
 import "fmt"
 
+// ListaPlanesSemanales guarda cada uno se los planes semanales de los usuarios.
+type ListaPlanesSemanales []PlanSemanal
+
 // PlanSemanal representa el plan semanal de rutinas de un usuario con un determinado tiempo libre.
 // Se comprueba que haya una rutina por día y que coincida la duración de cada rutina con el tiempo libre diario.
 type PlanSemanal []Rutina
