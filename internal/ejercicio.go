8cddc7d0b3382b83e01770544e45b4004e6b20fe
mmnuria <mmnuria@correo.ugr.es>
2025-01-12 21:46:24 +0100

    Soluciona #24 se necesita añadir un campo materiales e inicializar ejercicios para la implementación de la logica de negocio

--- internal/ejercicio.go
+++ internal/ejercicio.go
@@ -11,16 +11,28 @@ var ListaEjercicios []Ejercicio
 // - MinsEstimados: Tiempo estimado en minutos para realizar el ejercicio (máximo 1440, un día).
 // - ClaseEjercicio: Clase de ejercicio a realizar entre: Flexibilidad, Fuerza, Cardiovasculares, Calentamiento, Estiramiento.
 // - Intensidad: Intensidad del ejercicio entre: Baja, Media, Alta.
+// - Materiales: Especifica una lista de materiales necesarios para realizar ese ejercicio, puede no necesitar ninguno y estar vacía
 type Ejercicio struct {
 	Nombre         string
 	Descripcion    string
 	MinsEstimados  uint
 	ClaseEjercicio string
 	Intensidad     string
+	Materiales     []string
+}
+
+// init es una función especial que se ejecuta al inicio del paquete para inicializar las variables.
+func init() {
+	// Inicializamos ListaEjercicios con algunos ejercicios de ejemplo
+	ListaEjercicios = []Ejercicio{
+		{"Saltos", "Saltos en el lugar", 5, "Cardiovasculares", "Media", []string{}},
+		{"Flexiones", "Flexiones estándar", 10, "Fuerza", "Alta", []string{}},
+		{"Estiramientos", "Estiramiento básico de brazos", 5, "Estiramiento", "Baja", []string{}},
+	}
 }
 
 // NewEjercicio crea una nueva instancia de Ejercicio.
-func NewEjercicio(nombre string, descripcion string, minsEstimados uint, claseEjercicio string, intensidad string) (Ejercicio, error) {
+func NewEjercicio(nombre string, descripcion string, minsEstimados uint, claseEjercicio string, intensidad string, materiales []string) (Ejercicio, error) {
 	if minsEstimados <= 0 || minsEstimados > 1440 {
 		return Ejercicio{}, fmt.Errorf("el tiempo debe estar entre 1 y 1440 minutos, recibido: %d", minsEstimados)
 	}
@@ -42,5 +54,6 @@ func NewEjercicio(nombre string, descripcion string, minsEstimados uint, claseEj
 		MinsEstimados:  minsEstimados,
 		ClaseEjercicio: claseEjercicio,
 		Intensidad:     intensidad,
+		Materiales:     materiales,
 	}, nil
 }
