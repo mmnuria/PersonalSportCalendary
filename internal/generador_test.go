6be4ec4c54a9a2c5c5b29f4d99a19ce43f071d6f
mmnuria <mmnuria@correo.ugr.es>
2025-01-21 20:17:16 +0100

    Soluciona #23 cambio biblioteca de aserciones para los test

--- internal/generador_test.go
+++ internal/generador_test.go
@@ -3,72 +3,84 @@ package PersonalSportCalendary
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/gomega"
 )
 
 const TiempoValido = 10
 
 func TestGenerarRutina(t *testing.T) {
+	g := NewGomegaWithT(t)
+
 	rutina, err := GenerarRutina(TiempoValido)
-	assert.NoError(t, err)
-	assert.Equal(t, uint(TiempoValido), rutina.TiempoDuracion)
+	g.Expect(err).To(BeNil())
+	g.Expect(rutina.TiempoDuracion).To(Equal(uint(TiempoValido)))
 
 	totalTiempoEjercicios := uint(0)
 	for _, ejercicio := range rutina.Ejercicios {
 		totalTiempoEjercicios += ejercicio.MinsEstimados
 	}
-	assert.Equal(t, totalTiempoEjercicios, uint(TiempoValido))
+	g.Expect(totalTiempoEjercicios).To(Equal(uint(TiempoValido)))
 }
 
 func TestGenerarRutinaTiempoInvalido(t *testing.T) {
+	g := NewGomegaWithT(t)
+
 	const TiempoMinimo = 0
 	tiemposInvalidos := []uint{TiempoMinimo, MaxMinutosPorDia + 1}
 
 	for _, tiempo := range tiemposInvalidos {
 		_, err := GenerarRutina(tiempo)
-		assert.Error(t, err)
-		assert.Equal(t, "el tiempo disponible debe estar entre 1 y 1440 minutos", err.Error())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("el tiempo disponible debe estar entre 1 y 1440 minutos"))
 	}
 }
 
 func TestGenerarRutinaErrorPorFaltaDeEjercicios(t *testing.T) {
+	g := NewGomegaWithT(t)
+
 	const TiempoValido = 1000
 
 	_, err := GenerarRutina(TiempoValido)
-	assert.Error(t, err)
-	assert.Equal(t, "no hay suficientes ejercicios para llenar el tiempo disponible", err.Error())
+	g.Expect(err).ToNot(BeNil())
+	g.Expect(err.Error()).To(Equal("no hay suficientes ejercicios para llenar el tiempo disponible"))
 }
 
 func TestGenerarPlanSemanal(t *testing.T) {
+	g := NewGomegaWithT(t)
+
 	const DiasLibres = 3
 	tiempoLibre := TiempoLibre{DiasLibresSemanales: DiasLibres, TiempoLibreDiario: TiempoValido}
 	plan, err := GenerarPlanSemanal(tiempoLibre)
-	assert.NoError(t, err)
-	assert.Len(t, plan, DiasLibres)
+	g.Expect(err).To(BeNil())
+	g.Expect(plan).To(HaveLen(DiasLibres))
 }
 
 func TestGenerarPlanSemanalDuracion(t *testing.T) {
+	g := NewGomegaWithT(t)
+
 	const DiasLibres = 3
 	tiempoLibre := TiempoLibre{DiasLibresSemanales: DiasLibres, TiempoLibreDiario: TiempoValido}
 	plan, err := GenerarPlanSemanal(tiempoLibre)
-	assert.NoError(t, err)
+	g.Expect(err).To(BeNil())
 
 	for _, rutina := range plan {
-		assert.Equal(t, uint(TiempoValido), rutina.TiempoDuracion)
+		g.Expect(rutina.TiempoDuracion).To(Equal(uint(TiempoValido)))
 	}
 }
 
 func TestGenerarPlanSemanalEjercicios(t *testing.T) {
+	g := NewGomegaWithT(t)
+
 	const DiasLibres = 3
 	tiempoLibre := TiempoLibre{DiasLibresSemanales: DiasLibres, TiempoLibreDiario: TiempoValido}
 	plan, err := GenerarPlanSemanal(tiempoLibre)
-	assert.NoError(t, err)
+	g.Expect(err).To(BeNil())
 
 	for _, rutina := range plan {
 		totalTiempoEjercicios := uint(0)
 		for _, ejercicio := range rutina.Ejercicios {
 			totalTiempoEjercicios += ejercicio.MinsEstimados
 		}
-		assert.Equal(t, totalTiempoEjercicios, uint(TiempoValido))
+		g.Expect(totalTiempoEjercicios).To(Equal(uint(TiempoValido)))
 	}
 }
