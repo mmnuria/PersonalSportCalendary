6eba5aa8d34039116638cbacdc28aea9084c9e5d
mmnuria <mmnuria@correo.ugr.es>
2025-01-31 14:11:49 +0100

    Soluciona #40 añadir loggs en código

--- internal/generador_test.go
+++ internal/generador_test.go
@@ -3,6 +3,8 @@ package PersonalSportCalendary
 import (
 	"testing"
 
+	"PersonalSportCalendary/logger"
+
 	. "github.com/onsi/gomega"
 )
 
@@ -10,6 +12,10 @@ const TiempoValido = 10
 
 func TestGenerarRutina(t *testing.T) {
 	g := NewGomegaWithT(t)
+
+	logger.InitLogger("test_config", "yaml", "../")
+
+	logger.Logger.Infow("Ejecutando TestGenerarRutina", "TiempoValido", TiempoValido)
 
 	rutina, err := GenerarRutina(TiempoValido)
 	g.Expect(err).To(BeNil())
@@ -19,6 +25,7 @@ func TestGenerarRutina(t *testing.T) {
 	for _, ejercicio := range rutina.Ejercicios {
 		totalTiempoEjercicios += ejercicio.MinsEstimados
 	}
+	logger.Logger.Infow("Rutina generada correctamente", "TotalEjercicios", len(rutina.Ejercicios), "TotalTiempoEjercicios", totalTiempoEjercicios)
 	g.Expect(totalTiempoEjercicios).To(Equal(uint(TiempoValido)))
 }
 
@@ -29,6 +36,7 @@ func TestGenerarRutinaTiempoInvalido(t *testing.T) {
 	tiemposInvalidos := []uint{TiempoMinimo, MaxMinutosPorDia + 1}
 
 	for _, tiempo := range tiemposInvalidos {
+		logger.Logger.Warnw("Probando tiempo inválido", "TiempoIngresado", tiempo)
 		_, err := GenerarRutina(tiempo)
 		g.Expect(err).ToNot(BeNil())
 		g.Expect(err.Error()).To(Equal("el tiempo disponible debe estar entre 1 y 1440 minutos"))
@@ -37,12 +45,14 @@ func TestGenerarRutinaTiempoInvalido(t *testing.T) {
 
 func TestGenerarRutinaErrorPorFaltaDeEjercicios(t *testing.T) {
 	g := NewGomegaWithT(t)
-
+	logger.Logger.Infow("Iniciando prueba de generación de rutina por falta de ejercicios", "TiempoValido", TiempoValido)
 	const TiempoValido = 1000
 
 	_, err := GenerarRutina(TiempoValido)
 	g.Expect(err).ToNot(BeNil())
 	g.Expect(err.Error()).To(Equal("no hay suficientes ejercicios para llenar el tiempo disponible"))
+
+	logger.Logger.Infow("TestGenerarRutinaErrorPorFaltaDeEjercicios completado", "TiempoValido", TiempoValido)
 }
 
 func TestGenerarPlanSemanal(t *testing.T) {
@@ -50,9 +60,11 @@ func TestGenerarPlanSemanal(t *testing.T) {
 
 	const DiasLibres = 3
 	tiempoLibre := TiempoLibre{DiasLibresSemanales: DiasLibres, TiempoLibreDiario: TiempoValido}
+	logger.Logger.Infow("Iniciando generación del plan semanal", "DiasLibres", DiasLibres, "TiempoLibre", tiempoLibre)
 	plan, err := GenerarPlanSemanal(tiempoLibre)
 	g.Expect(err).To(BeNil())
 	g.Expect(plan).To(HaveLen(DiasLibres))
+	logger.Logger.Infow("Plan semanal generado correctamente", "DiasLibres", DiasLibres)
 }
 
 func TestGenerarPlanSemanalDuracion(t *testing.T) {
@@ -60,12 +72,16 @@ func TestGenerarPlanSemanalDuracion(t *testing.T) {
 
 	const DiasLibres = 3
 	tiempoLibre := TiempoLibre{DiasLibresSemanales: DiasLibres, TiempoLibreDiario: TiempoValido}
+	logger.Logger.Infow("Iniciando generación del plan semanal para verificar la duración", "DiasLibres", DiasLibres, "TiempoLibre", tiempoLibre)
 	plan, err := GenerarPlanSemanal(tiempoLibre)
 	g.Expect(err).To(BeNil())
 
 	for _, rutina := range plan {
+		logger.Logger.Infow("Verificando duración de rutina", "RutinaDuracion", rutina.TiempoDuracion)
 		g.Expect(rutina.TiempoDuracion).To(Equal(uint(TiempoValido)))
 	}
+
+	logger.Logger.Infow("Duración del plan semanal verificada correctamente", "DiasLibres", DiasLibres)
 }
 
 func TestGenerarPlanSemanalEjercicios(t *testing.T) {
@@ -73,6 +89,7 @@ func TestGenerarPlanSemanalEjercicios(t *testing.T) {
 
 	const DiasLibres = 3
 	tiempoLibre := TiempoLibre{DiasLibresSemanales: DiasLibres, TiempoLibreDiario: TiempoValido}
+	logger.Logger.Infow("Iniciando generación del plan semanal para verificar los ejercicios", "DiasLibres", DiasLibres, "TiempoLibre", tiempoLibre)
 	plan, err := GenerarPlanSemanal(tiempoLibre)
 	g.Expect(err).To(BeNil())
 
@@ -81,6 +98,8 @@ func TestGenerarPlanSemanalEjercicios(t *testing.T) {
 		for _, ejercicio := range rutina.Ejercicios {
 			totalTiempoEjercicios += ejercicio.MinsEstimados
 		}
+		logger.Logger.Infow("Verificando tiempo total de ejercicios de rutina", "TotalTiempoEjercicios", totalTiempoEjercicios)
 		g.Expect(totalTiempoEjercicios).To(Equal(uint(TiempoValido)))
 	}
+	logger.Logger.Infow("Ejercicios del plan semanal verificados correctamente", "DiasLibres", DiasLibres)
 }
